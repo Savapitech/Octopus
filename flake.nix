bdcada7b8ec96af311dda456b4a6aff41f9cbe74
titouanhct <titouan.hochet@epitech.eu>
2026-05-19 14:05:58 +0200

    Add first part of the project

---
+++ flake.nix
@@ -0,0 +1,33 @@
+{
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+
+  outputs = {
+    self,
+    nixpkgs,
+  }: let
+    forAllSystems = function:
+      nixpkgs.lib.genAttrs [
+        "x86_64-linux"
+        "aarch64-linux"
+      ] (system: function nixpkgs.legacyPackages.${system});
+  in {
+    devShells = forAllSystems (pkgs: {
+      default = pkgs.mkShell {
+        hardeningDisable = ["fortify"];
+        packages = with pkgs; [
+          valgrind
+          compiledb
+          clang
+          ansible
+        ];
+      };
+    });
+
+    formatter = forAllSystems (pkgs: pkgs.alejandra);
+
+    packages = forAllSystems (pkgs: {
+      default = self.packages.${pkgs.system}._scom;
+      _scom = pkgs.callPackage ./nix/package.nix { };
+    });
+  };
+}
